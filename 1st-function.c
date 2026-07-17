ee582a778008c7fddcef751b6bd899fa14854805
Garima-Namdev <garimanamdev30@gmail.com>
2026-07-17 16:04:16 +0530

    Add files via upload

---
+++ 1st-function.c
@@ -0,0 +1,25 @@
+<<<<<<< HEAD
+//to print a message by a function
+#include<stdio.h>
+void show();
+void show()
+{
+	printf("HELLO WORLD ");
+}
+void main()
+{
+	show();
+}
+=======
+//to print a message by a function
+#include<stdio.h> 
+void show();
+void show()
+{
+	printf("HELLO WORLD.... ");
+}
+void main() 
+{
+	show();
+}
+>>>>>>> a3b3a9cd7e3d97dc3357f9b679ba91d9f1a3bec6
