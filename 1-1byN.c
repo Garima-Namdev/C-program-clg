7208fa951fbcc35159bc9bb7a2802319eb8c7bd6
Garima-Namdev <garimanamdev30@gmail.com>
2026-07-17 16:03:27 +0530

    Add files via upload

---
+++ 1-1byN.c
@@ -0,0 +1,27 @@
+<<<<<<< HEAD
+//PRINT A SERIES 1 TO 1/n (loops)
+#include<stdio.h>
+void main()
+{
+	int n,i;
+	printf("Enter the value of n");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		printf(" 1/%d\t",i);// \t for space
+	}
+}
+=======
+//PRINT A SERIES 1 TO 1/N (loops)
+#include<stdio.h>
+void main()
+{
+	int n,i;
+	printf("Enter the value of n");
+	scanf("%d",&n); 
+	for(i=1;i<=n;i++)
+	{
+		printf(" 1/%d\t",i);// \t for space
+	}
+} 
+>>>>>>> a3b3a9cd7e3d97dc3357f9b679ba91d9f1a3bec6
