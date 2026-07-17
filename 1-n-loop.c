0e0b0e16919b9af3e43e3b62ff239d2574397bfd
Garima-Namdev <garimanamdev30@gmail.com>
2026-07-17 16:03:59 +0530

    Add files via upload

---
+++ 1-n-loop.c
@@ -0,0 +1,29 @@
+<<<<<<< HEAD
+
+//PRINT A NUMBER FROM 1 TO n
+#include<stdio.h>
+void main()
+{
+	int n,i;
+	printf("Enter the value of n");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		printf("%d\n",i);// \t for space
+	}
+}
+
+=======
+//PRINT A NUMBER FROM 1 TO n
+#include<stdio.h>
+void main()
+{
+	int n,i;
+	printf("Enter the value of n"); 
+	scanf("%d",&n);  
+	for(i=1;i<=n;i++)
+	{
+		printf("%d\t",i);// \t  for space 
+	}
+}
+>>>>>>> a3b3a9cd7e3d97dc3357f9b679ba91d9f1a3bec6
