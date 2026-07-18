522880efc85d3dba4dad07473f6d71ad18340dab
Garima-Namdev <garimanamdev30@gmail.com>
2026-07-18 15:40:13 +0530

    Add files via upload

---
+++ 3-digit-num.c
@@ -0,0 +1,35 @@
+<<<<<<< HEAD
+// to check a number is 3 digit or not
+#include<stdio.h>
+void main()
+{
+	int num;
+	printf("Enter a number:");
+	scanf("%d",&num);
+	if(num>99 && num<1000)
+	{
+		printf("This  is three digit number");
+	}
+	else
+	{
+		printf("Not a three digit number");
+	}
+}
+=======
+// to check a number is 3 digit or not
+#include<stdio.h>
+void main()
+{
+	int num;
+	printf("Enter a number:"); 
+	scanf("%d",&num);
+	if(num>99 && num<1000)
+	{
+		printf("This  is three digit number"); 
+	} 
+	else
+	{
+		printf("Not a three digit number");
+	}
+}
+>>>>>>> a3b3a9cd7e3d97dc3357f9b679ba91d9f1a3bec6
