29b3773ebbde98870c839804acc542b82eb4d628
Garima-Namdev <garimanamdev30@gmail.com>
2026-07-21 14:14:33 +0530

    Update 2d-array-first.c

--- 2d-array-first.c
+++ 2d-array-first.c
@@ -5,7 +5,7 @@ void main()
 	int r,c,i,j;
 	printf("enter the size of r and c\n");
 	scanf("%d%d",&r,&c);
-	int a[r][c];
+	int a[r][c]; 
 	printf("Enter the element of matrix\n");
 	for(i=0;i<r;i++)
 	{
