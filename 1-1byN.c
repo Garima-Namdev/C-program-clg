8e402019565fb698451c7561d658b4efcc145edc
Garima-Namdev <garimanamdev30@gmail.com>
2026-07-21 14:13:35 +0530

    Update 1-1byN.c

--- 1-1byN.c
+++ 1-1byN.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //PRINT A SERIES 1 TO 1/n (loops)
 #include<stdio.h>
 void main()
@@ -11,17 +10,3 @@ void main()
 		printf(" 1/%d\t",i);// \t for space
 	}
 }
-=======
-//PRINT A SERIES 1 TO 1/N (loops)
-#include<stdio.h>
-void main()
-{
-	int n,i;
-	printf("Enter the value of n");
-	scanf("%d",&n); 
-	for(i=1;i<=n;i++)
-	{
-		printf(" 1/%d\t",i);// \t for space
-	}
-} 
->>>>>>> a3b3a9cd7e3d97dc3357f9b679ba91d9f1a3bec6
